4360a25e711270b04d49e2a49b0c26be3f167e4a
isaac <isaac@10.17.138.58>
2023-10-30 13:57:40 -0500

    Got Update user to work

--- src/java/controllers/Private.java
+++ src/java/controllers/Private.java
@@ -2,7 +2,9 @@ package controllers;
 
 import business.Movie;
 import business.User;
+import business.Validation;
 import data.MovieDB;
+import data.security.SecurityUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -14,7 +16,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Private extends HttpServlet {
+
     String url = "";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -24,7 +30,7 @@ public class Private extends HttpServlet {
             response.sendRedirect("Public");
             return;
         }
-         
+
         String action = request.getParameter("action");
         if (action == null) {
             action = "default";
@@ -38,38 +44,55 @@ public class Private extends HttpServlet {
             }
             case "logout": {
                 logout(request);
-                
+
                 break;
             }
             case "updateUser": {
-                String newEmail = request.getParameter("email");
-                String newUserName = request.getParameter("userName");
-                String newPassword = request.getParameter("newPassword");
-                String re_enterPassword = request.getParameter("checkNewPassword");
-                String oldPassword = request.getParameter("oldPassword");
+                String errorEmail = "";
+                String errorNewPassword = "";
+                String errorOldPassword = "";
+                url = "/updateUser.jsp";
                 try {
-                    if (newPassword.equals(loggedInUser.getPassword())){
-                        url = "/updateUser.jsp";
+                    String newEmail = request.getParameter("email");
+                    String newUserName = request.getParameter("userName");
+                    if (!Validation.isEmail(newEmail)) {
+                        errorEmail = "The email you entered is not a valid format. A valid format looks like this: example@somesite.com";
+                        request.setAttribute("errorEmail", errorEmail);  
+                        break;
                     }
-                    else if (!newPassword.equals(re_enterPassword)){
-                        url = "/updateUser.jsp";
-                    }
-                    else if (!loggedInUser.getPassword().equals(oldPassword)){
-                        url = "/updateUser.jsp";
+                    String newPassword = request.getParameter("newPassword");
+                    String re_enterPassword = request.getParameter("checkNewPassword");
+                    String oldPassword = request.getParameter("oldPassword");
+                    if (newPassword.equals(re_enterPassword)) {
+                        newPassword = SecurityUtil.hashPassword(newPassword);
+                        if (newPassword.equals(loggedInUser.getPassword())) {
+                            url = "/updateUser.jsp";
+                        } else if (!SecurityUtil.isMatchingPassword(oldPassword, loggedInUser.getPassword())) {
+                            url = "/updateUser.jsp";
+                            errorOldPassword = "The password must be the same as the old password";
+                            request.setAttribute("errorOldPassword", errorOldPassword);
+                        } else {
+                            loggedInUser.setEmail(newEmail);
+                            loggedInUser.setUsername(newUserName);
+                            loggedInUser.setPassword(newPassword);
+                            MovieDB.updateUser(loggedInUser);
+                            url = "/userPage.jsp";
+                        }
                     }
                     else{
-                        loggedInUser.setEmail(newEmail);
-                        loggedInUser.setUsername(newUserName);
-                        loggedInUser.setPassword(newPassword);
-                        MovieDB.updateUser(loggedInUser);
+                        errorNewPassword = "The re-enter password must be the same as the New Password";
+                        request.setAttribute("errorNewPassword", errorNewPassword);
+                        
                     }
                 } catch (SQLException ex) {
                     Logger.getLogger(Private.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (Exception e) {
+                    Logger.getLogger(Private.class.getName()).log(Level.SEVERE, null, e);
                 }
                 break;
             }
             case "deleteAccount": {
-                /*int userID = 0;
+                int userID = 0;
                 try {
                     userID = loggedInUser.getUserID();
                 } catch (Exception e) {
@@ -78,10 +101,11 @@ public class Private extends HttpServlet {
                 try {
                     MovieDB.deleteUser(userID);
                     url = "/login.jsp";
+                    request.getSession().invalidate();
                 } catch (SQLException ex) {
                     Logger.getLogger(Private.class.getName()).log(Level.SEVERE, null, ex);
-                }*/
-                
+                }
+
                 break;
             }
             case "test": {
@@ -98,12 +122,11 @@ public class Private extends HttpServlet {
 
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
-    
+
     private void logout(HttpServletRequest request) {
         request.getSession().invalidate();
         url = "/Public?action=gotoIndex";
     }
-
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -145,20 +168,3 @@ public class Private extends HttpServlet {
     }// </editor-fold>
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
